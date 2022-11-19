ac0685b6582b956b15ad99263a4f32c6bfa79698
uxmain <bhuiyanmayn@gmail.com>
2022-11-20 00:36:11 +0600

    Services  completed

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import HeroSec from "../client/components/HeroSec/HeroSec";
 import Layout from "../client/components/layout/LAyout";
+import Services from "../client/components/Services/Services";
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <HeroSec />
+        <Services />
       </div>
     </Layout>
   );
