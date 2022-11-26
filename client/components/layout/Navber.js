27dac53fba60ce8fbe2149d4219f412e95efa9ae
MD : Sabbir Ahmed <sabbirahmednayeem941@gmail.com>
2022-11-26 23:33:39 +0600

    navber commit

--- client/components/layout/Navber.js
+++ client/components/layout/Navber.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./layoutStyle.module.css";
 import Image from "next/image";
 import moduleName from "../../../public/imges/logo-3.png";
@@ -6,92 +6,97 @@ import search from "../../../public/logo icon/icons8-search-50 (1).png";
 import users from "../../../public/logo icon/icons8-user-48.png";
 
 const Navber = () => {
+
+  const [navber, setNavber] = useState(true)
+
+
   return (
     <div className={styles.navbar_main}>
-      <div className={styles.menu_bar}>
-        <Image
-          style={{ borderRadius: "5px" }}
-          width={90}
-          height={25}
-          src={moduleName}
-          alt=""
-        />
-        <ul>
-          <li>
-            <a href="#">HOME</a>
-          </li>
-          <li>
-            <a href="#">DESTINATIONS</a>
-            <div className={styles.sub_menu_1}>
-              <ul>
-                <li>
-                  <a href="">DISTINATIONS1</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS2</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS DETAINS</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="#">TOURS</a>
-          </li>
-          <li>
-            <a href="#">PAGES</a>
-            <div className={styles.sub_menu_1}>
-              <ul>
-                <li>
-                  <a href="">DISTINATIONS1</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS2</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS DETAINS</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="#">ELEMENTS</a>
-            <div className={styles.sub_menu_1}>
-              <ul>
-                <li>
-                  <a href="">DISTINATIONS1</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS2</a>
-                </li>
-                <li>
-                  <a href="">DISTINATIONS DETAINS</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="#">BLOG</a>
-          </li>
-          <li>
-            <a href="#">CONTACT</a>
-          </li>
-        </ul>
+      <header className={styles.navber_header}>
+        <a href="" className={styles.logo}>
+          LOGO
+        </a>
 
+        <input type="checkbox" id={styles.manu_bar} />
+        <label htmlFor="" onClick={ () =>  setNavber(!navber)}>menu</label>
+
+       <nav className={styles.navber}>
+       { navber ?  <ul>
+            <li>
+              {" "}
+              <a href="">HOME</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="">DESTINATIONS</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="">TOURS + </a>
+              <ul>
+                <li>
+                  <a href="">blog</a>
+                </li>
+                <li>
+                  <a href="">blog +</a>
+                  <ul>
+                    <li>
+                      <a href="">html</a>
+                    </li>
+                    <li>
+                      <a href="">css</a>
+                    </li>
+                    <li>
+                      <a href="">java</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              {" "}
+              <a href="">PAGES</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="">ELEMENTS</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="">BLOG</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="">CONTACT</a>{" "}
+            </li>
+          </ul>
+          : ""
+
+        }
+        </nav> 
         <button className={styles.search_btn}>
           {" "}
-          <Image style={{padding:'6px'}} width={35} height={35} src={search} alt="" />
+          <Image
+            style={{ padding: "6px" }}
+            width={35}
+            height={35}
+            src={search}
+            alt=""
+          />
         </button>
         <button className={styles.users_btn}>
           {" "}
-          <Image style={{padding:'6px'}} width={35} height={35} src={users} alt="" />
+          <Image
+            style={{ padding: "6px" }}
+            width={35}
+            height={35}
+            src={users}
+            alt=""
+          />
         </button>
 
-        <button className={styles.book_a_tour}>
-          Book A Tour
-        </button>
-      </div>
+        <button className={styles.book_a_tour}>Book A Tour</button>
+      </header>
     </div>
   );
 };
